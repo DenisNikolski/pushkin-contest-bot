930867909a58e097a73757215fd2dcba8f8cd850
Den <den.nik32@gmail.com>
2017-12-30 00:02:22 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,33 +3,11 @@ lock '~> 3.10.1'
 
 set :application, 'pushkin-contest-bot'
 set :repo_url, 'https://github.com/DenisNikolski/pushkin-contest-bot.git'
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml}
-
-# Default value for linked_dirs is []
+set :deploy_to, '/var/www/pushkin/'
+set :linked_files, %w{config/database.yml config/secrets.yml config/secrets.yml.key}
 set :linked_dirs, %w{log tmp/pids public/assets tmp/cache tmp/sockets vendor/bundle}
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-set :ssh_options,{:forward_agent => true}
+set :ssh_options, { :forward_agent => true }
+set :pry, false
+set :rvm_ruby_version, '2.4.1@pushkin-bot'
+set :puma_preload_app, true
+set :puma_init_active_record, true
