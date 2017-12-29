7603cc33c9447ef5214c0b31b006b04ad0f25b78
Den <den.nik32@gmail.com>
2017-12-30 00:07:10 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,9 @@ lock '~> 3.10.1'
 set :application, 'pushkin-contest-bot'
 set :repo_url, 'https://github.com/DenisNikolski/pushkin-contest-bot.git'
 set :deploy_to, '/var/www/pushkin/'
-set :linked_files, %w{config/database.yml config/secrets.yml config/secrets.yml.key}
+set :linked_files, %w{config/database.yml config/secrets.yml }
 set :linked_dirs, %w{log tmp/pids public/assets tmp/cache tmp/sockets vendor/bundle}
 set :ssh_options, { :forward_agent => true }
 set :pry, false
-set :rvm_ruby_version, '2.4.1'
 set :puma_preload_app, true
 set :puma_init_active_record, true
