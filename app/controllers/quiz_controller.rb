10cabd387be4b8bf8f5c49e1693d4e41f79ca1b1
Den <den.nik32@gmail.com>
2017-12-30 17:03:04 +0300

    change quiz contr

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -3,9 +3,6 @@ require 'net/http'
 class QuizController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def create
-  end
-
   def index
     question = params[:question]
     id = params[:id]
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'main/index'
 
   resources :article
-  resources :quiz
+  resources :quiz, only: [:index]
 
   root 'main#index'
 end
