da424ffb8193c60f5b52f0621e459f8eac4b669e
Den <den.nik32@gmail.com>
2017-12-30 15:39:19 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '~> 3.10.1'
 
 set :application, 'pushkin-contest-bot'
 set :repo_url, 'https://github.com/DenisNikolski/pushkin-contest-bot.git'
-set :linked_files, %w{config/database.yml config/secrets.yml }
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{log tmp/pids public/assets tmp/cache tmp/sockets vendor/bundle}
 set :ssh_options, { :forward_agent => true }
 set :pry, false
