646f7296f4d3e10c777930a6db4a69842ffc2a26
Den <den.nik32@gmail.com>
2017-12-30 16:12:37 +0300

    quiz controller

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,4 +1,6 @@
 class QuizController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     question = params[:question]
     id = params[:id]
@@ -16,8 +18,8 @@ class QuizController < ApplicationController
       token: '0c68cd6461212cdc4b3694ca2df80d51',
       task_id: id
     }
-    Net::HTTP.post_form(uri, parameters)
-
+    response = Net::HTTP.post_form(uri, parameters)
+    render json: 'done'
     Result.create question: question, question_id: id, level: level, answer: answer
 
   end
